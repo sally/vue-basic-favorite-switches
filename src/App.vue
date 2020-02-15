9d63e3bac79cb8528823785f16e71a3a9d86e3d7
spark <parkyngj@gmail.com>
2020-02-14 20:27:45 -0800

    add switches data to app

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,64 @@ export default {
   name: 'App',
   components: {
     SwitchList
-  }
+  },
+  data() {
+    return {
+      switches: [
+        {
+          id: 1,
+          brand: 'Alpaca',
+          name: 'Alpaca',
+          type: 'Linear',
+          bottomOutForce: '62g',
+        },
+        {
+          id: 2,
+          brand: 'Alpaca',
+          name: 'Silent Alpaca',
+          type: 'Linear',
+          bottomOutForce: '62g',
+        },
+        {
+          id: 3,
+          brand: 'Gateron',
+          name: 'Clear',
+          type: 'Linear',
+          bottomOutForce: '45g',
+        },
+        {
+          id: 4,
+          brand: 'Gateron',
+          name: 'Yellow',
+          type: 'Linear',
+          bottomOutForce: '60g',
+        },
+        {
+          id: 5,
+          brand: 'Kailh',
+          name: 'BOX White',
+          type: 'Clicky',
+          bottomOutForce: '60g',
+        },
+        {
+          id: 6,
+          brand: 'Kailh',
+          name: 'BOX Pink',
+          type: 'Clicky',
+          bottomOutForce: '55g',
+        },
+        {
+          id: 7,
+          brand: 'Kailh',
+          name: 'Choc White',
+          type: 'Clicky',
+          travelDistance: '3mm',
+          actuationForce: '50g',
+          bottomOutForce: '50g',
+        },
+      ]
+    }
+  },
 }
 </script>
 
