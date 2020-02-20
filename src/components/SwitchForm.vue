24d664c3068b84f818d64a91922d86b5f5e18a1b
spark <parkyngj@gmail.com>
2020-02-19 21:16:27 -0800

    validate form for blank fields

--- src/components/SwitchForm.vue
+++ src/components/SwitchForm.vue
@@ -2,13 +2,17 @@
     <div id="switch-form">
         <div class="row">
             <div class="col-sm-12 col-md-6">
-                <form @submit="$emit('add:switch', mechSwitch)">
+                <form @submit.prevent="validateSwitch">
                     <div class="row">
                         <div class="col-sm-12 col-md-4">
                             <label for="brand">Brand</label>
                         </div>
                         <div class="col-sm-12 col-md-8">
-                            <input type="text" v-model="mechSwitch.brand" placeholder="e.g. 'Gateron'" />
+                            <input type="text"
+                                v-model="mechSwitch.brand"
+                                placeholder="e.g. 'Gateron'"
+                                v-bind:class="{ 'has-error': errors && invalidBrand }"
+                            />
                         </div>
                     </div>
 
@@ -17,7 +21,11 @@
                             <label for="name">Name</label>
                         </div>
                         <div class="col-sm-12 col-md-8">
-                            <input type="text" v-model="mechSwitch.name" placeholder="e.g. 'Brown'" />
+                            <input type="text"
+                                v-model="mechSwitch.name"
+                                placeholder="e.g. 'Brown'"
+                                v-bind:class="{ 'has-error': errors && invalidName }"
+                            />
                         </div>
                     </div>
 
@@ -26,7 +34,11 @@
                             <label for="type">Type</label>
                         </div>
                         <div class="col-sm-12 col-md-8">
-                            <input type="text" v-model="mechSwitch.type" placeholder="e.g. 'Tactile'" />
+                            <input type="text"
+                                v-model="mechSwitch.type"
+                                placeholder="e.g. 'Tactile'"
+                                v-bind:class="{ 'has-error': errors && invalidType }"
+                            />
                         </div>
                     </div>
 
@@ -35,13 +47,26 @@
                             <label for="bottomOutForce">Bottom-Out</label>
                         </div>
                         <div class="col-sm-12 col-md-8">
-                            <input type="text" v-model="mechSwitch.bottomOutForce" placeholder="e.g. '60g'" />
+                            <input type="text"
+                                v-model="mechSwitch.bottomOutForce"
+                                placeholder="e.g. '60g'"
+                                v-bind:class="{ 'has-error': errors && invalidBottomOut }"
+                            />
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="col-sm-12 col-md-offset-4 col-md-4">
                             <input type="submit" class="button tertiary" value="Add Switch" />
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-12 col-md-offset-4 col-md-4">
+                            <mark v-if="errors" class="secondary switch-form-error">
+                                <span class="icon-alert inverse"></span>
+                                Missing fields
+                            </mark>
                         </div>
                     </div>
                 </form>
@@ -55,6 +80,7 @@ export default {
     name: 'switch-form',
     data() {
         return {
+            errors: false,
             mechSwitch: {
                 brand: null,
                 name: null,
@@ -63,7 +89,40 @@ export default {
             },
         }
     },
+    computed: {
+        invalidBrand() {
+            return !this.mechSwitch.brand;
+        },
+        invalidName() {
+            return !this.mechSwitch.name;
+        },
+        invalidType() {
+            return !this.mechSwitch.type;
+        },
+        invalidBottomOut() {
+            return !this.mechSwitch.bottomOutForce;
+        },
+    },
+    methods: {
+        validateSwitch() {
+            if (this.invalidBrand || this.invalidName || this.invalidType || this.invalidBottomOut) {
+                this.errors = true;
+            } else {
+                this.errors = false;
+            }
+
+            !this.errors && this.$emit('add:switch', this.mechSwitch);
+        }
+    }
 }
 </script>
 
-<style scoped />
+<style scoped>
+    .switch-form-error {
+        margin: 8px;
+    }
+
+    .has-error {
+        border: 1px solid #d92121;
+    }
+</style>
