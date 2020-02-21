5e77655e2dbece061a9fb99411bd52c3f546b26f
spark <parkyngj@gmail.com>
2020-02-20 19:19:57 -0800

    fix indentation in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
-    <h1>Sally's Favorite Mechanical Switches</h1>
-    <switch-form
-      @add:switch="addSwitch"
-    />
-    <switch-list :switches="switches"
-      @update:switch="updateSwitch"
-      @delete:switch="deleteSwitch"
-    />
-  </div>
+    <div id="app">
+        <h1>Sally's Favorite Mechanical Switches</h1>
+        <switch-form
+          @add:switch="addSwitch"
+        />
+        <switch-list :switches="switches"
+          @update:switch="updateSwitch"
+          @delete:switch="deleteSwitch"
+        />
+    </div>
 </template>
 
 <script>
@@ -18,93 +18,93 @@ import SwitchList from './components/SwitchList.vue';
 const uuidv4 = require('uuid/v4');
 
 export default {
-  name: 'App',
-  components: {
-    SwitchForm,
-    SwitchList,
-  },
-  data() {
-    return {
-      switches: [
-        {
-          id: uuidv4(),
-          name: 'Alpaca',
-          type: 'Linear',
-          bottomOutForce: '62g',
-        },
-        {
-          id: uuidv4(),
-          name: 'Silent Alpaca',
-          type: 'Linear',
-          bottomOutForce: '62g',
-        },
-        {
-          id: uuidv4(),
-          name: 'Gateron Clear',
-          type: 'Linear',
-          bottomOutForce: '45g',
-        },
-        {
-          id: uuidv4(),
-          name: 'Gateron Yellow',
-          type: 'Linear',
-          bottomOutForce: '60g',
-        },
-        {
-          id: uuidv4(),
-          name: 'Kailh BOX White',
-          type: 'Clicky',
-          bottomOutForce: '60g',
-        },
-        {
-          id: uuidv4(),
-          name: 'Kailh BOX Pink',
-          type: 'Clicky',
-          bottomOutForce: '55g',
-        },
-        {
-          id: uuidv4(),
-          name: 'Kailh Choc White',
-          type: 'Clicky',
-          bottomOutForce: '50g',
-        },
-      ]
-    }
-  },
-  methods: {
-    addSwitch(switchToAdd) {
-      const newSwitch = {
-        ...switchToAdd,
-        id: uuidv4(),
-      }
-      this.switches.push(newSwitch);
+    name: 'App',
+    components: {
+        SwitchForm,
+        SwitchList,
     },
-    updateSwitch(switchToUpdate) {
-      this.switch = this.switches.map(mechSwitch => {
-        mechSwitch.id === switchToUpdate.id ? switchToUpdate : mechSwitch;
-      })
+    data() {
+        return {
+            switches: [
+                {
+                    id: uuidv4(),
+                    name: 'Alpaca',
+                    type: 'Linear',
+                    bottomOutForce: '62g',
+                },
+                {
+                    id: uuidv4(),
+                    name: 'Silent Alpaca',
+                    type: 'Linear',
+                    bottomOutForce: '62g',
+                },
+                {
+                    id: uuidv4(),
+                    name: 'Gateron Clear',
+                    type: 'Linear',
+                    bottomOutForce: '45g',
+                },
+                {
+                    id: uuidv4(),
+                    name: 'Gateron Yellow',
+                    type: 'Linear',
+                    bottomOutForce: '60g',
+                },
+                {
+                    id: uuidv4(),
+                    name: 'Kailh BOX White',
+                    type: 'Clicky',
+                    bottomOutForce: '60g',
+                },
+                {
+                    id: uuidv4(),
+                    name: 'Kailh BOX Pink',
+                    type: 'Clicky',
+                    bottomOutForce: '55g',
+                },
+                {
+                    id: uuidv4(),
+                    name: 'Kailh Choc White',
+                    type: 'Clicky',
+                    bottomOutForce: '50g',
+                },
+            ]
+        }
     },
-    deleteSwitch(idToDelete) {
-      this.switches = this.switches.filter(mechSwitch => mechSwitch.id != idToDelete);
+    methods: {
+        addSwitch(switchToAdd) {
+            const newSwitch = {
+              ...switchToAdd,
+              id: uuidv4(),
+            }
+            this.switches.push(newSwitch);
+        },
+        updateSwitch(switchToUpdate) {
+            this.switch = this.switches.map(mechSwitch => {
+              mechSwitch.id === switchToUpdate.id ? switchToUpdate : mechSwitch;
+            })
+        },
+        deleteSwitch(idToDelete) {
+            this.switches = this.switches.filter(mechSwitch => mechSwitch.id != idToDelete);
+        }
     }
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+    #app {
+      font-family: Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
 
-#app td {
-  text-overflow: clip;
-}
+    #app td {
+      text-overflow: clip;
+    }
 
-#app input[type='text'] {
-  width: 100%;
-}
+    #app input[type='text'] {
+      width: 100%;
+    }
 
     .fade-enter-active, .fade-leave-active {
         transition: opacity .5s;
